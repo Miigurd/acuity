afcd77c0dae666917080a4f7caea622f374d5623
Kirt Asia Mag-usara <162999637+Miigurd@users.noreply.github.com>
2026-09-04 17:08:04 +0800

    fix: export unverify_business from services package

--- acuity-backend/webapp/services/__init__.py
+++ acuity-backend/webapp/services/__init__.py
@@ -1,5 +1,5 @@
 from .business_service import get_business_by_id, get_all_businesses, get_paginated_businesses, update_businesses, flag_business, rollback_business
-from .bplo_service import upload_bplo_csv, get_bplo_queue, approve_bplo_match, reject_bplo_match
+from .bplo_service import upload_bplo_csv, get_bplo_queue, approve_bplo_match, reject_bplo_match, unverify_business
 from .edit_service import get_held_edits, approve_held_edit, reject_held_edit
 from .search_service import search_businesses, track_interaction_event
 
@@ -14,6 +14,7 @@ __all__ = [
     'get_bplo_queue',
     'approve_bplo_match',
     'reject_bplo_match',
+    'unverify_business',
     'get_held_edits',
     'approve_held_edit',
     'reject_held_edit',
